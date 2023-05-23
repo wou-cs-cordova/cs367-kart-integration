9f9c071e9b16f4fcc230cc60e236b9f222233954
Lucas Cordova <lpcordova@outlook.com>
2023-05-22 19:21:32 -0700

    Reset

--- SmartKart.Web/Repositories/Interfaces/IContactRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using SmartKart.Web.Models;
-
-namespace SmartKart.Web.Repositories.Interfaces;
-
-public interface IContactRepository
-{
-    Task<Contact> SendMessage(Contact contact);
-    Task<Contact> Subscribe(string address);
-}
--- SmartKart.Web/Repositories/Interfaces/IOrderRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using SmartKart.Web.Models;
-
-namespace SmartKart.Web.Repositories.Interfaces;
-
-public interface IOrderRepository
-{
-    Task<Order> CheckOut(Order order);
-    Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
-}
